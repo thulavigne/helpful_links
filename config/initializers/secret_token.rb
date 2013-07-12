bfc999ddb266ff4c261ada2176edf6b82b299efd
Thu Quach <thuquach90@gmail.com>
2013-07-12 16:39:04 -0700

    Generate the Rails 3 - Helpful Links application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelpfulLinks::Application.config.secret_token = '825679df8b8191612aeabe593dd36dcfe9c98f27831316bc94ff063a20d58464ed10bd9287c830c6a734f2a6c28697484b6a5d6fbb7f3567107f65e5c7b7bfed'
